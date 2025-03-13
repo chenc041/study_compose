b26ad008180741824c0b80da8aaea325f279d1ad
chenc <double_cl@163.com>
2025-03-13 22:02:56 +0800

    feat(utils): 添加文件存储工具类
    - 新增 FileStorageUtils 类，用于提供文件存储相关功能- 该类通过构造函数注入应用上下文，便于进行文件操作

---
+++ app/src/main/java/site/chenc/study_compose/utils/FileStorageUtils.kt
@@ -0,0 +1,9 @@
+package site.chenc.study_compose.utils
+
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+class FileStorageUtils constructor(
+    @ApplicationContext private val context: Context
+) {
+}
